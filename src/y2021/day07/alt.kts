e1a4585d65d454bb16f3332175d794fe3bc96ea6
Kingsley Adio <adiksonline@gmail.com>
2021-12-10 00:27:06 +0100

    Added day 7 solutions

---
+++ src/y2021/day07/alt.kts
@@ -0,0 +1,32 @@
+import java.io.File
+
+fun part1(input: List<Int>): Int {
+    val min = input.minOrNull()!!
+    val max = input.maxOrNull()!!
+    val plane = IntArray(max - min + 1)
+    for (value in input) plane[value - min]++
+
+    val aligningForward = IntArray(plane.size)
+    var subs = plane[0]
+    for (i in 1..plane.lastIndex) {
+        aligningForward[i] = aligningForward[i - 1] + subs
+        subs += plane[i]
+    }
+    val aligningBackward = IntArray(plane.size)
+    subs = plane.last()
+    for (i in plane.lastIndex - 1 downTo 0) {
+        aligningBackward[i] = aligningBackward[i + 1] + subs
+        subs += plane[i]
+    }
+    for (i in aligningBackward.indices) aligningForward[i] += aligningBackward[i]
+    return aligningForward.minOrNull()!!
+}
+
+fun main() {
+    val input = File("input.txt").useLines { lines ->
+        lines.first().split(",").map { it.toInt() }
+    }
+    println(part1(input))
+}
+
+main()
