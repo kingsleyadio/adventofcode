767d75ade1148a130facda4309c420b1e8c967cf
Kingsley Adio <adiksonline@gmail.com>
2022-12-12 20:09:59 +0100

    Add day 4 solutions

---
+++ src/y2022/day04/solution.kts
@@ -0,0 +1,17 @@
+import java.io.File
+
+fun main() {
+    solution { a, b, x, y -> a in x..y && b in x..y || x in a..b && y in a..b }
+    solution { a, b, x, y -> a in x..y || b in x..y || x in a..b || y in a..b }
+}
+
+fun solution(predicate: (Int, Int, Int, Int) -> Boolean) {
+    var result = 0
+    File("input.txt").forEachLine { line ->
+        val (a, b, x, y) = line.split(",").flatMap { it.split("-") }.map { it.toInt() }
+        if (predicate(a, b, x, y)) result++
+    }
+    println(result)
+}
+
+main()
