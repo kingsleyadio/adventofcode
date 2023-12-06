38bd752819019a99249d42b6e44226d1b5f64f38
Kingsley Adio <adiksonline@gmail.com>
2023-12-06 19:12:43 +0100

    Optimize day05

--- src/com/kingsleyadio/adventofcode/y2023/Day05.kt
+++ src/com/kingsleyadio/adventofcode/y2023/Day05.kt
@@ -1,88 +1,70 @@
 package com.kingsleyadio.adventofcode.y2023
 
-import java.io.File
+import com.kingsleyadio.adventofcode.util.readInput
 
 fun main() {
-    val (seeds, cultivator) = File("input.txt").useLines { sequence ->
+    val (seeds, pipeline) = readInput(2023, 5).useLines { sequence ->
         val lines = sequence.iterator()
         val seeds = lines.next().substringAfter(": ").split(" ").map { it.toLong() }
         lines.next()
-        lines.next()
-        val seedToSoil = lines.generateMap()
-        lines.next()
-        val soilToFertilizer = lines.generateMap()
-        lines.next()
-        val fertilizerToWater = lines.generateMap()
-        lines.next()
-        val waterToLight = lines.generateMap()
-        lines.next()
-        val lightToTemperature = lines.generateMap()
-        lines.next()
-        val temperatureToHumidity = lines.generateMap()
-        lines.next()
-        val humidityToLocation = lines.generateMap()
-
-        seeds to Cultivator(
-            seedToSoil,
-            soilToFertilizer,
-            fertilizerToWater,
-            waterToLight,
-            lightToTemperature,
-            temperatureToHumidity,
-            humidityToLocation
-        )
-    }
-    part1(seeds, cultivator)
-    part2(seeds, cultivator)
-}
-
-private fun part1(seeds: List<Long>, cultivator: Cultivator) {
-    val lowestLocation = seeds.minOf(cultivator::calculateSeedLocation)
-    println(lowestLocation)
-}
-
-private fun part2(spec: List<Long>, cultivator: Cultivator) {
-    // Using asSequence here to avoid OOM
-    val seeds = spec.asSequence().chunked(2).flatMap { (start, size) -> start..<start+size }
-    val lowestLocation = seeds.minOf(cultivator::calculateSeedLocation)
-    println(lowestLocation)
-}
-
-class Cultivator(
-    private val seedToSoil: Map<LongRange, Long>,
-    private val soilToFertilizer: Map<LongRange, Long>,
-    private val fertilizerToWater: Map<LongRange, Long>,
-    private val waterToLight: Map<LongRange, Long>,
-    private val lightToTemperature: Map<LongRange, Long>,
-    private val temperatureToHumidity: Map<LongRange, Long>,
-    private val humidityToLocation: Map<LongRange, Long>,
-) {
-    fun calculateSeedLocation(seed: Long): Long {
-        val soil = seedToSoil.getValue(seed)
-        val fertilizer = soilToFertilizer.getValue(soil)
-        val water = fertilizerToWater.getValue(fertilizer)
-        val light = waterToLight.getValue(water)
-        val temperature = lightToTemperature.getValue(light)
-        val humidity = temperatureToHumidity.getValue(temperature)
-        return humidityToLocation.getValue(humidity)
-    }
-
-    private fun Map<LongRange, Long>.getValue(key: Long): Long {
-        for ((range, v) in this) {
-            if (key in range) {
-                val diff = key - range.first
-                return v + diff
+        val pipeline = buildList {
+            repeat(7) {
+                lines.next()
+                add(lines.generateMap())
             }
         }
-        return key
+        seeds to pipeline
     }
+    part1(seeds, pipeline)
+    part2(seeds, pipeline)
 }
 
-fun Iterator<String>.generateMap(): Map<LongRange, Long> = buildMap {
+private fun part1(seeds: List<Long>, pipeline: List<Map<LongRange, Long>>) {
+    val lowestLocation = seeds.minOf { seed ->
+        pipeline.fold(seed) { number, map -> map.getValue(number) }
+    }
+    println(lowestLocation)
+}
+
+private fun part2(spec: List<Long>, pipeline: List<Map<LongRange, Long>>) {
+    val lowestLocation = spec.chunked(2).minOf { (start, size) ->
+        var seed = start
+        var min = Long.MAX_VALUE
+        while(seed < start + size) {
+            var current = seed
+            var increment = Long.MAX_VALUE
+            pipeline.forEach { map ->
+                for ((range, dest) in map) {
+                    if (current in range) {
+                        increment = minOf(increment, range.last - current)
+                        current = dest + current - range.first
+                        return@forEach
+                    }
+                }
+            }
+            min = minOf(min, current) // current -> location
+            seed += (increment + 1).coerceAtLeast(1)
+        }
+        min
+    }
+    println(lowestLocation)
+}
+
+private fun Iterator<String>.generateMap(): Map<LongRange, Long> = buildMap {
     while (hasNext()) {
         val line = next()
         if (line.isEmpty()) break
         val (dest, src, range) = line.split(" ").map { it.toLong() }
         put(src..<src + range, dest)
     }
+}
+
+private fun Map<LongRange, Long>.getValue(key: Long): Long {
+    for ((range, v) in this) {
+        if (key in range) {
+            val diff = key - range.first
+            return v + diff
+        }
+    }
+    return key
 }
