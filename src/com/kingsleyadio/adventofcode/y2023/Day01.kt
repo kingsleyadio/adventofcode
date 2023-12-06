efeec05da76719bff612a2c5dec88442cf6f1151
Kingsley Adio <adiksonline@gmail.com>
2023-12-06 19:53:12 +0100

    Slightly improve day01

--- src/com/kingsleyadio/adventofcode/y2023/Day01.kt
+++ src/com/kingsleyadio/adventofcode/y2023/Day01.kt
@@ -19,39 +19,21 @@ private fun part1() {
 
 private fun part2() {
     var sum = 0
-    val numbers = listOf("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
     readInput(2023, 1).forEachLine { line ->
-        val first = run {
-            val numIndex = line.indexOfFirst { it.isDigit() }
-            val numNumber = line[numIndex].digitToInt()
-            val view = line.subSequence(0..numIndex)
-            var wordNumber = 0
-            var wordIndex = numIndex
-            numbers.forEachIndexed { index, num -> 
-                val lineIndex = view.indexOf(num)
-                if (lineIndex in 0..<wordIndex) {
-                    wordNumber = index
-                    wordIndex = lineIndex
-                }
-            }
-            if (wordIndex < numIndex) wordNumber else numNumber
-        }
-        val last = run {
-            val numIndex = line.indexOfLast { it.isDigit() }
-            val numNumber = line[numIndex].digitToInt()
-            val view = line.substring(numIndex+1)
-            var wordNumber = 0
-            var wordIndex = -1
-            numbers.forEachIndexed { index, num ->
-                val lineIndex = view.lastIndexOf(num)
-                if (lineIndex > wordIndex) {
-                    wordNumber = index
-                    wordIndex = lineIndex
-                }
-            }
-            if (wordIndex + numIndex + 1 > numIndex) wordNumber else numNumber
-        }
+        val first = findNumber(line, reversed = false)
+        val last = findNumber(line, reversed = true)
         sum += first * 10 + last
     }
     println(sum)
+}
+
+private val NUMBERS = listOf("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+private fun findNumber(line: String, reversed: Boolean): Int {
+    val progression = line.indices.run { if (reversed) reversed() else this }
+    for (i in progression) {
+        val char = line[i]
+        if (char.isDigit()) return char.digitToInt()
+        NUMBERS.forEachIndexed { index, num -> if (line.startsWith(num, i)) return index }
+    }
+    error("Not found")
 }
