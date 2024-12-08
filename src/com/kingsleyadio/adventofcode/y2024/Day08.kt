11ecb253693e15b4170b2666dc0361835771b81d
Kingsley Adio <adiksonline@gmail.com>
2024-12-08 10:13:31 +0100

    Add day08 solutions

---
+++ src/com/kingsleyadio/adventofcode/y2024/Day08.kt
@@ -0,0 +1,38 @@
+package com.kingsleyadio.adventofcode.y2024
+
+import com.kingsleyadio.adventofcode.util.readInput
+
+fun main() {
+    val input = readInput(2024, 8, false).readLines()
+    val boundary = Rect(0..input[0].lastIndex, 0..input.lastIndex)
+    val frequencies = hashMapOf<Char, MutableList<Index>>()
+    for (y in input.indices) {
+        val row = input[y]
+        for (x in row.indices) if (row[x] != '.') frequencies.getOrPut(row[x]) { mutableListOf() }.add(Index(x, y))
+    }
+    part1(frequencies, boundary)
+    part2(frequencies, boundary)
+}
+
+private fun part1(frequencies: Map<Char, List<Index>>, boundary: Rect) = solve(frequencies) { a, b ->
+    val diff = a - b
+    for (antinode in arrayOf(a + diff, b - diff)) if (antinode in boundary) add(antinode)
+}
+
+private fun part2(frequencies: Map<Char, List<Index>>, boundary: Rect) = solve(frequencies) { a, b ->
+    add(a)
+    add(b)
+    val diff = a - b
+    val antinodes = arrayOf(a + diff, b - diff)
+    while (antinodes[0] in boundary) { add(antinodes[0]); antinodes[0] += diff }
+    while (antinodes[1] in boundary) { add(antinodes[1]); antinodes[1] -= diff }
+}
+
+private inline fun solve(frequencies: Map<Char, List<Index>>, find: MutableSet<Index>.(a: Index, b: Index) -> Unit) {
+    val antinodes = hashSetOf<Index>()
+    for ((_, v) in frequencies) {
+        if (v.size < 2) continue
+        for (i in 0..<v.lastIndex) for (j in i + 1..v.lastIndex) antinodes.find(v[i], v[j])
+    }
+    println(antinodes.size)
+}
