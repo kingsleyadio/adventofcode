bd5b18c258983df49311d25c1130e3eb21cea9ba
Kingsley Adio <adiksonline@gmail.com>
2024-12-10 11:16:01 +0100

    Slight adjustments to day10

--- src/com/kingsleyadio/adventofcode/y2024/Day10.kt
+++ src/com/kingsleyadio/adventofcode/y2024/Day10.kt
@@ -3,42 +3,35 @@ package com.kingsleyadio.adventofcode.y2024
 import com.kingsleyadio.adventofcode.util.readInput
 
 fun main() {
-    val input = readInput(2024, 10, false).readLines().map { it.toCharArray().map(Char::digitToInt) }
-    val bounds = Rect(0..input[0].lastIndex, 0..input.lastIndex)
-    part1(input, bounds)
-    part2(input, bounds)
+    val input = readInput(2024, 10).readLines().map { it.toCharArray().map(Char::digitToInt) }
+    part1(input)
+    part2(input)
 }
 
-private fun part1(input: List<List<Int>>, bounds: Rect) {
+private fun part1(input: List<List<Int>>) {
+    val trails = hashSetOf<Pair<Index, Index>>()
+    solve(input) { a, b -> trails.add(a to b) }
+    println(trails.size)
+}
+
+private fun part2(input: List<List<Int>>) {
     var result = 0
-    for (y in input.indices) {
-        val row = input[y]
-        for (x in row.indices) if (row[x] == 0) {
-            val unique = hashSetOf<Index>()
-            walk(input, bounds, Index(x, y), unique::add)
-            result += unique.size
+    solve(input) { _, _ -> result++ }
+    println(result)
+}
+
+private fun solve(input: List<List<Int>>, onTrail: (Index, Index) -> Unit) {
+    val bounds = Rect(0..input[0].lastIndex, 0..input.lastIndex)
+    fun walk(start: Index, current: Index = start) {
+        val value = input[current.y][current.x]
+        if (value == 9) return onTrail(start, current)
+        for (direction in Directions.Cardinal) {
+            val next = current + direction
+            if (next in bounds && input[next.y][next.x] == value + 1) walk(start, next)
         }
     }
-    println(result)
-}
-
-private fun part2(input: List<List<Int>>, bounds: Rect) {
-    var result = 0
     for (y in input.indices) {
         val row = input[y]
-        for (x in row.indices) if (row[x] == 0) walk(input, bounds, Index(x, y)) { result++ }
-    }
-    println(result)
-}
-
-private fun walk(input: List<List<Int>>, bounds: Rect, current: Index, onFinish: (Index) -> Unit) {
-    val value = input[current.y][current.x]
-    if (value == 9) return onFinish(current)
-    for (i in dx.indices) {
-        val next = current + Index(dx[i], dy[i])
-        if (next in bounds && input[next.y][next.x] == value + 1) walk(input, bounds, next, onFinish)
+        for (x in row.indices) if (row[x] == 0) walk(Index(x, y))
     }
 }
-
-private val dx = intArrayOf(0, 1, 0, -1)
-private val dy = intArrayOf(-1, 0, 1, 0)
