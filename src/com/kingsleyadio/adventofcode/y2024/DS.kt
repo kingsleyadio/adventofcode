839b247d93c9fa651f2102792fb2f943eaf268ad
Kingsley Adio <adiksonline@gmail.com>
2024-12-10 11:44:23 +0100

    Add Directions utility

--- src/com/kingsleyadio/adventofcode/y2024/DS.kt
+++ src/com/kingsleyadio/adventofcode/y2024/DS.kt
@@ -6,3 +6,18 @@ operator fun Index.minus(other: Index) = Index(x - other.x, y - other.y)
 
 data class Rect(val x: IntRange, val y: IntRange)
 operator fun Rect.contains(other: Index) = other.y in y && other.x in x
+
+object Directions {
+    @JvmField val N = Index(0, -1)
+    @JvmField val S = Index(0, 1)
+    @JvmField val E = Index(1, 0)
+    @JvmField val W = Index(-1, 0)
+    @JvmField val NE = Index(1, -1)
+    @JvmField val NW = Index(-1, -1)
+    @JvmField val SE = Index(1, 1)
+    @JvmField val SW = Index(-1, 1)
+
+    @JvmField val Cardinal = listOf(N, S, E, W)
+    @JvmField val InterCardinal = listOf(NE, SE, NW, SW)
+    @JvmField val Compass = Cardinal + InterCardinal
+}
