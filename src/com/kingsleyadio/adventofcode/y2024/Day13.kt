f097d7f2ad592c0f9f1a5dd2564e2d676fb67552
Kingsley Adio <adiksonline@gmail.com>
2024-12-13 16:48:16 +0100

    Slight improvements to day13

--- src/com/kingsleyadio/adventofcode/y2024/Day13.kt
+++ src/com/kingsleyadio/adventofcode/y2024/Day13.kt
@@ -1,8 +1,6 @@
 package com.kingsleyadio.adventofcode.y2024
 
 import com.kingsleyadio.adventofcode.util.readInput
-import kotlin.math.floor
-import kotlin.math.roundToLong
 
 fun main() {
     val input = readInput(2024, 13, false).readLines()
@@ -14,37 +12,28 @@ private fun part1(input: List<String>) = solve(input, 0L)
 private fun part2(input: List<String>) = solve(input, 10000000000000L)
 
 private fun solve(input: List<String>, offset: Long) {
-    val machines = buildMachines(input, offset)
-    val cost = machines.map { m -> m.movesForB().let { b -> Pair(m.movesForA(b), b) } }.sumOf { (a, b) -> a * 3 + b }
+    val cost = buildMachines(input, offset).sumOf(Machine::cost)
     println(cost)
 }
 
-private fun buildMachines(input: List<String>, priceOffset: Long): List<Machine> = buildList {
-    val lines = input.iterator()
-    while (lines.hasNext()) {
-        val (ax, ay) = REGEX_B.matchEntire(lines.next())!!.groupValues.drop(1).map(String::toInt)
-        val (bx, by) = REGEX_B.matchEntire(lines.next())!!.groupValues.drop(1).map(String::toInt)
-        val (px, py) = REGEX_P.matchEntire(lines.next())!!.groupValues.drop(1).map(String::toLong)
-        add(Machine(Button(ax, ay), Button(bx, by), Price(priceOffset + px, priceOffset + py)))
-        if (lines.hasNext()) lines.next()
-    }
+private fun buildMachines(input: List<String>, priceOffset: Long): List<Machine> = input.chunked(4) { (a, b, p) ->
+    val (ax, ay) = NUMBER.findAll(a).map { it.value.toInt() }.toList()
+    val (bx, by) = NUMBER.findAll(b).map { it.value.toInt() }.toList()
+    val (px, py) = NUMBER.findAll(p).map { it.value.toLong() }.toList()
+    Machine(Button(ax, ay), Button(bx, by), Price(priceOffset + px, priceOffset + py))
 }
 
 private class Button(val x: Int, val y: Int)
 private class Price(val x: Long, val y: Long)
 private class Machine(val a: Button, val b: Button, val p: Price) {
-
-    fun movesForB(): Long {
-        val moveB = (p.y * a.x - p.x * a.y) / (a.x * b.y - a.y * b.x).toDouble()
-        return if (moveB == floor(moveB)) moveB.roundToLong() else 0
-    }
-
-    fun movesForA(moveB: Long): Long {
-        if (moveB == 0L) return 0
-        val moveA = (p.x - moveB * b.x) / a.x.toDouble()
-        return if (moveA == floor(moveA)) moveA.roundToLong() else -1
+    fun cost(): Long {
+        val bn = p.y * a.x - p.x * a.y
+        val bd = a.x * b.y - a.y * b.x
+        val moveB = if (bn % bd == 0L) bn / bd else return 0
+        val an = p.x - moveB * b.x
+        val moveA = if (an % a.x == 0L) an / a.x else return 0
+        return moveA * 3 + moveB
     }
 }
 
-private val REGEX_B = "Button [AB]: X\\+(\\d+), Y\\+(\\d+)".toRegex()
-private val REGEX_P = "Prize: X=(\\d+), Y=(\\d+)".toRegex()
+private val NUMBER = "\\d+".toRegex()
