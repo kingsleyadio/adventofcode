7500ed4171b3b2e09925d010c759f8c440f2ca7a
Kingsley Adio <adiksonline@gmail.com>
2024-12-22 13:08:29 +0100

    Improve runtime of day22 (slight memory cost)

--- src/com/kingsleyadio/adventofcode/y2024/Day22.kt
+++ src/com/kingsleyadio/adventofcode/y2024/Day22.kt
@@ -3,46 +3,32 @@ package com.kingsleyadio.adventofcode.y2024
 import com.kingsleyadio.adventofcode.util.*
 
 fun main() {
-    val input = readInput(2024, 22).readLines().map(String::toInt)
+    val input = readInput(2024, 22).readLines().map(String::toLong)
     part1(input)
     part2(input)
 }
 
-private fun part1(input: List<Int>) {
-    val result = input.sumOf { calculateSecret(it.toLong(), 2000) }
+private fun part1(input: List<Long>) {
+    val result = input.sumOf { calculateSecret(it).elementAt(2000) }
     println(result)
 }
 
-private fun part2(input: List<Int>) {
-    val sequences = input.map(::viableSaleSequences)
-    val visited = hashSetOf<List<Int>>()
-    var max = Int.MIN_VALUE
-    for (sequence in sequences) for ((k, _) in sequence) if (visited.add(k)) {
-        val sum = sequences.sumOf { it.getOrDefault(k, 0) }
-        max = maxOf(max, sum)
-    }
-    println(max)
+private fun part2(input: List<Long>) {
+    val map = IntArray(608850)
+    for (n in input) for ((k, v) in sequences(n)) map[k] += v
+    println(map.max())
 }
 
-private fun viableSaleSequences(secret: Int, rounds: Int = 2000): Map<List<Int>, Int> {
-    val list = mutableListOf(secret % 10)
-    var current = secret.toLong()
-    for (i in 1..rounds) {
-        current = calculateSecret(current, 1)
-        list.add((current % 10L).toInt())
-    }
-    return buildMap {
-        val diffs = list.zipWithNext { a, b -> b - a }
-        for ((i, d) in diffs.withIndex()) if (i >= 3 && d > 0) putIfAbsent(diffs.slice(i - 3..i), list[i + 1])
-    }
+private fun sequences(secret: Long) = buildMap {
+    val list = calculateSecret(secret).take(2000).map { (it % 10).toInt() }.toList()
+    val diffs = list.zipWithNext { a, b -> b - a }
+    for ((i, d) in diffs.withIndex()) if (i >= 3 && d > 0) putIfAbsent(diffs.subList(i - 3, i + 1).toKey(), list[i + 1])
 }
 
-private fun calculateSecret(start: Long, rounds: Int): Long {
-    var current = start
-    repeat(rounds) {
-        current = (current xor (current shl 6)).mod(16777216L)
-        current = (current xor (current shr 5)).mod(16777216L)
-        current = (current xor (current shl 11)).mod(16777216L)
-    }
-    return current
+private fun calculateSecret(start: Long) = generateSequence(start) { prev ->
+    var next = prev xor (prev shl 6) and 0xffffffL
+    next = next xor (next shr 5) and 0xffffffL
+    next xor (next shl 11) and 0xffffffL
 }
+
+private fun List<Int>.toKey() = (this[0] + 9 shl 15) + (this[1] + 9 shl 10) + (this[2] + 9 shl 5) + (this[3] + 9)
