9fd6b5d5975b92b5cc576e78e3f8f473ce47ec40
Kingsley Adio <adiksonline@gmail.com>
2025-12-08 19:30:28 +0100

    Cleanup day08 solutions

--- src/com/kingsleyadio/adventofcode/y2025/Day08.kt
+++ src/com/kingsleyadio/adventofcode/y2025/Day08.kt
@@ -13,59 +13,50 @@ fun main() {
     part2(input)
 }
 
-private fun part1(input: List<List<Int>>, isSample: Boolean) {
-    val pq = PriorityQueue<Day08Pair> { a, b -> compareValues(a.distance, b.distance) }
-    for (i in 0..<input.lastIndex) {
-        val from = input[i]
-        for (j in (i+1)..input.lastIndex) {
-            val to = input[j]
-            pq.add(Day08Pair(from, to, distance(from, to)))
-        }
-    }
-    var circuitId = 0
-    val circuitToBox = hashMapOf<Int, Set<List<Int>>>()
-    val boxToCircuit = hashMapOf<List<Int>, Int>()
-    for (box in input) {
-        val id = circuitId++
-        boxToCircuit[box] = id
-        circuitToBox[id] = setOf(box)
-    }
-    var attempts = 0
-    while(attempts++ < if (isSample) 10 else 1000) {
-        val (from, to) = pq.poll()
-        val fromCircuit = boxToCircuit.getValue(from)
-        val fromFamily = circuitToBox.remove(fromCircuit).orEmpty()
-        val toCircuit = boxToCircuit.getValue(to)
-        val toFamily = circuitToBox.remove(toCircuit).orEmpty()
-        if (fromFamily.size > toFamily.size) {
-            toFamily.forEach { boxToCircuit[it] = fromCircuit }
-            circuitToBox[fromCircuit] = fromFamily + toFamily
-        } else {
-            fromFamily.forEach { boxToCircuit[it] = toCircuit }
-            circuitToBox[toCircuit] = fromFamily + toFamily
-        }
-    }
-    val result = circuitToBox.values.map { it.size }.sorted().takeLast(3).reduce(Int::times)
+private fun part1(input: List<Coord>, isSample: Boolean) {
+    val relationship = input.buildRelationship()
+    relationship.connectCircuit(if (isSample) 10 else 1000)
+    val result = relationship.first.values.map { it.size }.sorted().takeLast(3).reduce(Int::times)
     println(result)
 }
 
-private fun part2(input: List<List<Int>>) {
-    val pq = PriorityQueue<Day08Pair> { a, b -> compareValues(a.distance, b.distance) }
-    for (i in 0..<input.lastIndex) {
-        val from = input[i]
-        for (j in (i+1)..input.lastIndex) {
-            val to = input[j]
-            pq.add(Day08Pair(from, to, distance(from, to)))
-        }
+private fun part2(input: List<Coord>) {
+    val relationship = input.buildRelationship()
+    val (from, to) = relationship.connectCircuit(relationship.third.size)
+    val result = from[0] * to[0].toLong()
+    println(result)
+}
+
+private typealias Coord = List<Int>
+private typealias Day08Edge = Triple<Coord, Coord, Double> // from, to, distance
+private typealias Relationship = Triple<MutableMap<Int, Set<Coord>>, MutableMap<Coord, Int>, PriorityQueue<Day08Edge>>
+
+private fun distance(from: Coord, to: Coord): Double {
+    val (x1, y1, z1) = from
+    val (x2, y2, z2) = to
+    return sqrt(abs(x1 - x2).toDouble().pow(2) + abs(y1 - y2).toDouble().pow(2) + abs(z1 - z2).toDouble().pow(2))
+}
+
+private fun List<Coord>.buildRelationship(): Relationship {
+    val pq = PriorityQueue<Day08Edge> { a, b -> compareValues(a.third, b.third) }
+    for (i in 0..<lastIndex) {
+        val from = this[i]
+        for (j in (i+1)..lastIndex) pq.add(Day08Edge(from, this[j], distance(from, this[j])))
     }
     var circuitId = 0
-    val circuitToBox = hashMapOf<Int, Set<List<Int>>>()
-    val boxToCircuit = hashMapOf<List<Int>, Int>()
-    for (box in input) {
+    val circuitToBox = hashMapOf<Int, Set<Coord>>()
+    val boxToCircuit = hashMapOf<Coord, Int>()
+    for (box in this) {
         val id = circuitId++
         boxToCircuit[box] = id
         circuitToBox[id] = setOf(box)
     }
+    return Triple(circuitToBox, boxToCircuit, pq)
+}
+
+private fun Relationship.connectCircuit(maxAttempts: Int): Pair<Coord, Coord> {
+    val (circuitToBox, boxToCircuit, pq) = this
+    var attempts = 0
     while(true) {
         val (from, to) = pq.poll()
         val fromCircuit = boxToCircuit.getValue(from)
@@ -79,17 +70,6 @@ private fun part2(input: List<List<Int>>) {
             fromFamily.forEach { boxToCircuit[it] = toCircuit }
             circuitToBox[toCircuit] = fromFamily + toFamily
         }
-        if (circuitToBox.size == 1) {
-            val result = from[0] * to[0].toLong()
-            println(result)
-            break
-        }
+        if (++attempts == maxAttempts || circuitToBox.size == 1) return Pair(from, to)
     }
-}
-
-private data class Day08Pair(val from: List<Int>, val to: List<Int>, val distance: Double)
-private fun distance(from: List<Int>, to: List<Int>): Double {
-    val (x1, y1, z1) = from
-    val (x2, y2, z2) = to
-    return sqrt(abs(x1 - x2).toDouble().pow(2) + abs(y1 - y2).toDouble().pow(2) + abs(z1 - z2).toDouble().pow(2))
 }
